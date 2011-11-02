e96d28b89e4f9e0513b976381e11adcf19a5ff16
Raj Aththanayake <rajitha_kba@yahoo.com.au>
2011-11-02 21:57:42 +1100

    Add WinForms lib and VS Extension
    Signed-off-by: Raj Aththanayake <rajitha_kba@yahoo.com.au>

---
+++ AspxToRazorForms/RazorForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace AspxToRazorForms
+{
+    partial class RazorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RazorForm));
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblSaveLocation = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnBrowseDir = new System.Windows.Forms.Button();
+            this.dirErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.fileErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGenerateRazor = new System.Windows.Forms.Button();
+            this.rdoOriginalSave = new System.Windows.Forms.RadioButton();
+            this.rdoAlternativeSave = new System.Windows.Forms.RadioButton();
+            this.rdoOriginalToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.rdoAltLocationTip = new System.Windows.Forms.ToolTip(this.components);
+            this.successPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dirErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.successPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select \r\nWeb Forms View(s)";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(558, 14);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(36, 26);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblSaveLocation
+            // 
+            this.lblSaveLocation.AutoSize = true;
+            this.lblSaveLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaveLocation.Location = new System.Drawing.Point(36, 299);
+            this.lblSaveLocation.Name = "lblSaveLocation";
+            this.lblSaveLocation.Size = new System.Drawing.Size(102, 18);
+            this.lblSaveLocation.TabIndex = 4;
+            this.lblSaveLocation.Text = "Save Location";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDir.Location = new System.Drawing.Point(145, 297);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(406, 24);
+            this.txtDir.TabIndex = 5;
+            // 
+            // btnBrowseDir
+            // 
+            this.btnBrowseDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseDir.Location = new System.Drawing.Point(558, 297);
+            this.btnBrowseDir.Name = "btnBrowseDir";
+            this.btnBrowseDir.Size = new System.Drawing.Size(36, 26);
+            this.btnBrowseDir.TabIndex = 10;
+            this.btnBrowseDir.Text = "...";
+            this.btnBrowseDir.UseVisualStyleBackColor = true;
+            this.btnBrowseDir.Click += new System.EventHandler(this.btnBrowseDir_Click);
+            // 
+            // dirErrorProvider
+            // 
+            this.dirErrorProvider.ContainerControl = this;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(145, 15);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.ScrollAlwaysVisible = true;
+            this.checkedListBox1.Size = new System.Drawing.Size(406, 199);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 13;
+            // 
+            // fileErrorProvider
+            // 
+            this.fileErrorProvider.ContainerControl = this;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Image = global::AspxToRazorForms.Properties.Resources.refreshi_256;
+            this.btnReset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReset.Location = new System.Drawing.Point(145, 344);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(188, 52);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnGenerateRazor
+            // 
+            this.btnGenerateRazor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerateRazor.Image = global::AspxToRazorForms.Properties.Resources.icon_processi;
+            this.btnGenerateRazor.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGenerateRazor.Location = new System.Drawing.Point(360, 344);
+            this.btnGenerateRazor.Name = "btnGenerateRazor";
+            this.btnGenerateRazor.Size = new System.Drawing.Size(191, 52);
+            this.btnGenerateRazor.TabIndex = 9;
+            this.btnGenerateRazor.Text = "Convert";
+            this.btnGenerateRazor.UseVisualStyleBackColor = true;
+            this.btnGenerateRazor.Click += new System.EventHandler(this.btnGenerateRazor_Click);
+            // 
+            // rdoOriginalSave
+            // 
+            this.rdoOriginalSave.AutoSize = true;
+            this.rdoOriginalSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.rdoOriginalSave.Location = new System.Drawing.Point(145, 230);
+            this.rdoOriginalSave.Name = "rdoOriginalSave";
+            this.rdoOriginalSave.Size = new System.Drawing.Size(283, 22);
+            this.rdoOriginalSave.TabIndex = 15;
+            this.rdoOriginalSave.TabStop = true;
+            this.rdoOriginalSave.Text = "Save to the original location(s) [Default]";
+            this.rdoOriginalToolTip.SetToolTip(this.rdoOriginalSave, "This option will allow you to save CSHTML files to their corresponding WebForms f" +
+        "ile(s) location(s).");
+            this.rdoOriginalSave.UseVisualStyleBackColor = true;
+            // 
+            // rdoAlternativeSave
+            // 
+            this.rdoAlternativeSave.AutoSize = true;
+            this.rdoAlternativeSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.rdoAlternativeSave.Location = new System.Drawing.Point(145, 265);
+            this.rdoAlternativeSave.Name = "rdoAlternativeSave";
+            this.rdoAlternativeSave.Size = new System.Drawing.Size(217, 22);
+            this.rdoAlternativeSave.TabIndex = 16;
+            this.rdoAlternativeSave.TabStop = true;
+            this.rdoAlternativeSave.Text = "Save to an altenative location";
+            this.rdoAltLocationTip.SetToolTip(this.rdoAlternativeSave, "This option will allow you to save CSHTML file to a specified location.");
+            this.rdoAlternativeSave.UseVisualStyleBackColor = true;
+            this.rdoAlternativeSave.CheckedChanged += new System.EventHandler(this.rdoAlternativeSave_CheckedChanged);
+            // 
+            // successPictureBox
+            // 
+            this.successPictureBox.Image = global::AspxToRazorForms.Properties.Resources.iconi_go;
+            this.successPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("successPictureBox.InitialImage")));
+            this.successPictureBox.Location = new System.Drawing.Point(558, 344);
+            this.successPictureBox.Name = "successPictureBox";
+            this.successPictureBox.Size = new System.Drawing.Size(48, 50);
+            this.successPictureBox.TabIndex = 18;
+            this.successPictureBox.TabStop = false;
+            // 
+            // RazorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(622, 426);
+            this.Controls.Add(this.successPictureBox);
+            this.Controls.Add(this.rdoAlternativeSave);
+            this.Controls.Add(this.rdoOriginalSave);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnBrowseDir);
+            this.Controls.Add(this.btnGenerateRazor);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.lblSaveLocation);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RazorForm";
+            this.Text = "WebForms(ASPX/ASCX) to Razor Views (CSHTML) Converter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dirErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.successPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblSaveLocation;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnGenerateRazor;
+        private System.Windows.Forms.Button btnBrowseDir;
+        private System.Windows.Forms.ErrorProvider dirErrorProvider;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ErrorProvider fileErrorProvider;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.RadioButton rdoOriginalSave;
+        private System.Windows.Forms.RadioButton rdoAlternativeSave;
+        private System.Windows.Forms.ToolTip rdoOriginalToolTip;
+        private System.Windows.Forms.ToolTip rdoAltLocationTip;
+        private System.Windows.Forms.PictureBox successPictureBox;
+    }
+}
+
---
+++ AspxToRazorForms/RazorForm.cs
@@ -0,0 +1,277 @@
+﻿namespace AspxToRazorForms
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using System.IO;
+    using System.Text;
+    using System.Windows.Forms;
+    using Telerik.RazorConverter;
+    using Telerik.RazorConverter.Razor.DOM;
+    using System.ComponentModel.Composition.Hosting;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Razor form
+    /// </summary>
+    public partial class RazorForm : Form
+    {
+        /// <summary>
+        /// Gets or sets the parser.
+        /// </summary>
+        /// <value>......
+        /// The parser.
+        /// </value>
+        [Import]
+        private IWebFormsParser Parser
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the converter.hhhhhhhh
+        /// </summary>
+        /// <value>
+        /// The converter.
+        /// </value>
+        [Import]
+        private IWebFormsConverter<IRazorNode> Converter
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the renderer.
+        /// </summary>
+        /// <value>
+        /// The renderer.
+        /// </value>
+        [Import]
+        private IRenderer<IRazorNode> Renderer
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RazorForm"/> class.
+        /// </summary>
+        public RazorForm()
+        {
+            InitializeComponent();
+            successPictureBox.Visible = false;
+            var catalog = new AssemblyCatalog(typeof(IWebFormsParser).Assembly);
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+            rdoOriginalSave.Checked = true;
+            rdoAlternativeSave.Checked = false;
+
+            txtDir.Visible = false;
+            btnBrowseDir.Visible = false;
+            lblSaveLocation.Visible = false;
+        }
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var OpenWebFormFileDialog = new OpenFileDialog();
+
+            if (checkedListBox1.Items != null && checkedListBox1.Items.Count > 0)
+            {
+                string lastPath = checkedListBox1.Items[checkedListBox1.Items.Count - 1].ToString();
+                OpenWebFormFileDialog.InitialDirectory = lastPath;
+            }
+            else
+            {
+                OpenWebFormFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            }
+
+            OpenWebFormFileDialog.Filter = "ASPX files (*.aspx)|*.aspx|ASCX fIles (*.ascx)|*.ascx";
+            OpenWebFormFileDialog.FilterIndex = 1;
+            OpenWebFormFileDialog.RestoreDirectory = true;
+            OpenWebFormFileDialog.Multiselect = true;
+
+            if (OpenWebFormFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileErrorProvider.Clear();
+
+                try
+                {
+                    foreach (String file in OpenWebFormFileDialog.FileNames)
+                    {
+                        if (!checkedListBox1.Items.Contains(file))
+                        {
+                            checkedListBox1.Items.Add(file);
+                        }
+                    }
+
+                    if (checkedListBox1.Items.Count > 0)
+                    {
+                        for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                        {
+                            checkedListBox1.SetItemChecked(i, true);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+
+        private void btnBrowseDir_Click(object sender, EventArgs e)
+        {
+            var folderBrowserDialog1 = new FolderBrowserDialog {Description = "Razor File save location"};
+
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                string firstItem = checkedListBox1.CheckedItems[0].ToString();
+                FileInfo info = new FileInfo(firstItem);
+                folderBrowserDialog1.SelectedPath = info.DirectoryName;
+            }
+
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dirErrorProvider.Clear();
+
+                if (!string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                {
+                    txtDir.Text = folderBrowserDialog1.SelectedPath;
+
+                }
+            }
+        }
+
+
+        private void btnGenerateRazor_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count <= 0)
+            {
+                fileErrorProvider.SetError(btnBrowse, "No files to convert.");
+                return;
+            }
+
+            const string errorMessage = "Unable to convert Web Forms files";
+
+            if (rdoOriginalSave.Checked)
+            {
+                try
+                {
+                    GenerateRazorFilesForDefaultLocation(ConvertAndStoreRazorFiles, checkedListBox1);
+                }
+                catch (Exception exc)
+                {
+                    throw new Exception(errorMessage, exc.InnerException);
+                }
+            }
+            else
+            {
+                string directoryName = txtDir.Text;
+
+                if (string.IsNullOrEmpty(directoryName))
+                {
+                    dirErrorProvider.SetError(btnBrowseDir, "Please specify a directory name.");
+                    return;
+                }
+
+                if (!Directory.Exists(directoryName))
+                {
+                    dirErrorProvider.SetError(btnBrowseDir, "Invalid directory name.");
+                    return;
+                }
+
+                try
+                {
+                    GenerateRazorFilesForUserSpecifiedDirectory(ConvertAndStoreRazorFiles, directoryName, checkedListBox1);
+                }
+                catch (Exception exc)
+                {
+                    throw new Exception(errorMessage, exc.InnerException);
+                }
+
+                System.Diagnostics.Process.Start(directoryName);
+            }
+
+            successPictureBox.Visible = true;
+        }
+
+
+        private static void GenerateRazorFilesForUserSpecifiedDirectory(Action<string, string, string, string> delegateConvertAndStoreRazorFiles, string directoryName, CheckedListBox checkedListBox1)
+        {
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                delegateConvertAndStoreRazorFiles(item.ToString(), directoryName, Path.GetDirectoryName(item.ToString()), Path.GetFileName(item.ToString()));
+            }
+        }
+
+        private static void GenerateRazorFilesForDefaultLocation(Action<string, string, string, string> delegateConvertAndStoreRazorFiles, CheckedListBox checkedListBox1)
+        {
+            foreach (var checkedItem in checkedListBox1.CheckedItems)
+            {
+                string item = checkedItem.ToString();
+                var fileInfo = new FileInfo(item);
+
+                if (fileInfo.Exists)
+                {
+                    //both input/output directories are the same.
+                    delegateConvertAndStoreRazorFiles(item, fileInfo.DirectoryName, fileInfo.DirectoryName, Path.GetFileName(item));
+                }
+            }
+        }
+
+
+
+        private void ConvertAndStoreRazorFiles(string fileToConvert, string outputDirectory, string inputDirectory, string inputFilter)
+        {
+            foreach (var file in Directory.GetFiles(inputDirectory, inputFilter))
+            {
+                var webFormsPageSource = File.ReadAllText(file, Encoding.UTF8);
+                var webFormsDocument = Parser.Parse(webFormsPageSource);
+                var razorDom = Converter.Convert(webFormsDocument);
+                var razorPage = Renderer.Render(razorDom);
+
+                var outputFile = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(file) + ".cshtml");
+                File.WriteAllText(outputFile, razorPage, Encoding.UTF8);
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            txtDir.Text = string.Empty;
+            successPictureBox.Visible = false;
+            fileErrorProvider.Clear();
+            dirErrorProvider.Clear();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void rdoAlternativeSave_CheckedChanged(object sender, EventArgs e)
+        {
+            successPictureBox.Visible = false;
+
+            if (rdoAlternativeSave.Checked)
+            {
+                txtDir.Visible = true;
+                btnBrowseDir.Visible = true;
+                lblSaveLocation.Visible = true;
+                
+            }
+            else
+            {
+                txtDir.Visible = false;
+                btnBrowseDir.Visible = false;
+                lblSaveLocation.Visible = false;
+            }
+        }
+    }
+}
