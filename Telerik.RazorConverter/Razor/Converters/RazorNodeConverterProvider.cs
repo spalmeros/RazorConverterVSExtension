4a74bdb8ae6a44bfeda5461cdca8923d0acb00ae
Raj Aththanayake <rajitha_kba@yahoo.com.au>
2011-11-02 21:40:30 +1100

    initial commit
    Signed-off-by: Raj Aththanayake <rajitha_kba@yahoo.com.au>

---
+++ Telerik.RazorConverter/Razor/Converters/RazorNodeConverterProvider.cs
@@ -0,0 +1,37 @@
+﻿namespace Telerik.RazorConverter.Razor.Converters
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Telerik.RazorConverter;
+    using Telerik.RazorConverter.Razor.DOM;
+
+    [Export(typeof(IRazorNodeConverterProvider))]
+    public class RazorNodeConverterProvider : IRazorNodeConverterProvider
+    {
+        [ImportingConstructor]
+        public RazorNodeConverterProvider(  IRazorDirectiveNodeFactory directiveNodeFactory,
+                                            IRazorSectionNodeFactory sectionNodeFactory,
+                                            IRazorCodeNodeFactory codeNodeFactory,
+                                            IRazorTextNodeFactory textNodeFactory,
+                                            IRazorCommentNodeFactory commentNodeFactory,
+                                            IRazorExpressionNodeFactory expressionNodeFactory,
+                                            IContentTagConverterConfiguration contentTagConverterConfig)
+        {
+            NodeConverters = new INodeConverter<IRazorNode>[] {
+                new DirectiveConverter(directiveNodeFactory),
+                new ContentTagConverter(this, sectionNodeFactory, contentTagConverterConfig),
+                new CodeGroupConverter(this),
+                new CodeBlockConverter(codeNodeFactory),
+                new TextNodeConverter(textNodeFactory),
+                new CommentNodeConverter(commentNodeFactory),
+                new ExpressionBlockConverter(expressionNodeFactory)
+            };
+        }
+
+        public IList<INodeConverter<IRazorNode>> NodeConverters
+        {
+            get;
+            private set;
+        }
+    }
+}
