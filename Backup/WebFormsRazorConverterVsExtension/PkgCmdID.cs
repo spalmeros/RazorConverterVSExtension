9230295d26aa619acd15dda32cc91c80ebcaadb3
Dana Benson <danabenson@gmail.com>
2013-07-23 13:51:31 -0700

    converted to 2012

---
+++ Backup/WebFormsRazorConverterVsExtension/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace RajAththanayake.WebFormsRazorConverterVsExtension
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+
+
+    };
+}
